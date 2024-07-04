7eff8b0342f5bc03418e321cf090bdc61fc626d2
AlexandrePpereira <131178619+AlexandrePpereira@users.noreply.github.com>
2024-07-04 09:50:08 -0300

    Add files via upload

---
+++ provareact/prova/src/components/Principal.jsx
@@ -0,0 +1,14 @@
+import Header from "./Cabeca"
+import Clubes from "./Lista"
+
+export default function Home(){
+return(
+<>
+<div className="App">
+<Header nome="Alexandre Pereira" Turma="Turma: 3B" />
+<Clubes />
+</div>
+</>
+)
+}
+
